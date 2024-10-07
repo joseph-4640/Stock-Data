98a5fd0178b93391fcd00769826e3dade1ee01d3
Joseph Sunday <obierosunday126@gmail.com>
2024-10-07 09:35:09 +0300

    Modifications on py file

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# "/hello" path with 200 status code
+@app.get("/hello", status_code=200)
+def hello():
+    """Return dictionary with greetin message
+    """
+    
+    return {"message": "Hello world!"}
