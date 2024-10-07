9986da7d8eb45e3667aca2b47d37281e8e3e98a2
Joseph Sunday <obierosunday126@gmail.com>
2024-10-07 09:40:37 +0300

    Modifications on main.py file

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ from fastapi import FastAPI
 from model import GarchModel
 from pydantic import BaseModel
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d5415fc0024b9e49d5ae3606013c9a93a404d87a
 app = FastAPI()
 
 # "/hello" path with 200 status code
@@ -18,23 +15,19 @@ def hello():
     """Return dictionary with greetin message
     """
     
-<<<<<<< HEAD
-    return {"message": "Hello world!"}
-=======
     return {"message": "Hello world!"}
 
 # "/fit" path
 # FitIn class
 class FitIn(BaseModel):
-    ticker:str
+    ticker: str
     use_new_data:bool
     n_observations: int
-    p:int
-    q:int
+    p: int
+    q: int
 
 # FitOut Class
 
 class FitOut(FitIn):
     success: bool
     message: str
->>>>>>> d5415fc0024b9e49d5ae3606013c9a93a404d87a
